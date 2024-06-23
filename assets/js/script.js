f1251b97c4fa80ef4977db4d503f9f6687ebbb30
Vishnu Rathod <vishnurathod5884@gmail.com>
2024-06-23 12:21:03 +0530

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Machine Learning", "Data Science", "web designing", "android development", "web development"],
+    strings: ["Machine Learning", "Data Science", "web designing", "project development", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
